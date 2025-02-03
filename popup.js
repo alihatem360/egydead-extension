dcb578d4edaeb4fc217d0dfb3421feefb228d584
Ali Hatem <alihatemramadan5@gmail.com>
2025-02-03 20:43:25 +0200

    Update extension icon path, enhance URL matching, and add README documentation

--- popup.js
+++ popup.js
@@ -16,7 +16,8 @@ button.addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (
         tabs[0]?.url?.includes(
-          "egydeadw.sbs/category/%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D9%83%D8%B1%D8%AA%D9%88%D9%86"
+          "egydeadw.sbs/category/%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D9%83%D8%B1%D8%AA%D9%88%D9%86",
+          "egydeadw.sbs/category/%d8%a7%d9%81%d9%84%d8%a7%d9%85-%d9%83%d8%b1%d8%aa%d9%88%d9%86/"
         )
       ) {
         chrome.tabs.sendMessage(tabs[0].id, {
